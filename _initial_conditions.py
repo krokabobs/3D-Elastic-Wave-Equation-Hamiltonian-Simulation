7cabcbc995696461683bbe78916c8c1d038e190d
Isaac No <83660750+isaacyno@users.noreply.github.com>
2026-04-12 00:44:13 -0700

    Updated gaussian_IC function to be consistent

--- _initial_conditions.py
+++ _initial_conditions.py
@@ -13,7 +13,8 @@ def gaussian_IC(Nx,Ny,Nz,dx,dy,dz,xmin,xmax,ymin,ymax,zmin,zmax):
     
     N_main,N_vx,N_vy,N_vz,N_sxy,N_sxz,N_syz,N_vel,N_stress,psi_len = get_grid_size(Nx,Ny,Nz)
 
-    phi_0 = np.zeros(psi_len, dtype=complex)
+    #phi_0 = np.zeros(psi_len, dtype=complex)
+    phi_0 = np.zeros(psi_len)
 
     x = np.linspace(xmin,xmax,Nx)
     y = np.linspace(ymin,ymax,Ny)
@@ -21,10 +22,8 @@ def gaussian_IC(Nx,Ny,Nz,dx,dy,dz,xmin,xmax,ymin,ymax,zmin,zmax):
     # Build grid directly in (Nz, Ny, Nx) order
     Z, Y, X = np.meshgrid(z, y, x, indexing='ij')
 
-    sigma = 1.5 * dx
+    sigma = 0.25 * min(xmax - xmin, ymax - ymin, zmax - zmin)
     amplitude = -.5
-
-    zmin = (Nz - 1) * dz / 2
 
     # Gaussian centered at (0,0,-zmin)
     gaussian = amplitude * np.exp(-(X**2 + Y**2 + (Z + zmin)**2) / (2 * sigma**2))
